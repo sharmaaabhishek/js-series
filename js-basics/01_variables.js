7c88789bb1d6a7af4ab8b47b6713e2d6cb10ae11
Abhishek Sharma <119608669+sharmaaabhishek@users.noreply.github.com>
2025-03-28 17:25:14 +0000

    Variables in javascript

---
+++ js-basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 98765;
+let accountFullName = "Abhishek Sharma";
+var accountCity= "Mumbai";
+accountEmail = "demo@mail.com";
+let accountState;
+
+// accountId = 896868; // Not allowed assigning to constant variable
+
+accountFullName = "Abhishek Adbal";
+accountCity = "Maharashtra";
+accountEmail = "account@mail.com";
+
+
+console.log(accountId);
+
+console.table([accountId,accountFullName,accountCity, accountEmail,accountState]);
